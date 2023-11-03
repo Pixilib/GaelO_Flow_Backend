8631d3859b7fb4c028520d9af2aac12330e86ad8
Antonin <antonin.laudon@epitech.eu>
2023-11-03 12:35:22 +0100

    Add: Role auth

---
+++ GaelO-Flow/src/roles/roles.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Roles } from './roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get(Roles, context.getHandler());
+    if (!roles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    for (const role of roles) {
+        if (role == user.role.name)
+            return true;
+    }
+    return false;
+  }
+}
