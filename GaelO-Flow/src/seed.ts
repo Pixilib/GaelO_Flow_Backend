cbc76885d787aa7d1f72d1850976a2728a51a8bb
Salim Kanoun <salim.kanoun@gmail.com>
2023-12-06 23:07:33 +0100

    try fix

--- GaelO-Flow/src/seed.ts
+++ GaelO-Flow/src/seed.ts
@@ -1,15 +1,15 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { SeedService } from "./seeder.service";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SeedService } from './seeder.service';
 
 const seeder = async () => {
-    const app = await NestFactory.createApplicationContext(AppModule);
-    const seedservice = app.get(SeedService);
+  const app = await NestFactory.createApplicationContext(AppModule);
+  const seedservice = app.get(SeedService);
 
-    await seedservice.seed();
-    await app.close();
+  await seedservice.seed();
+  await app.close();
 
-    console.log("Seeding complete");
+  console.log('Seeding complete');
 };
 
 seeder();
