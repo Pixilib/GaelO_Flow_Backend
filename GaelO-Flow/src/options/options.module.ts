17a8c3cb54910af94d43f33e94f2703327384a66
Antonin <antonin.laudon@epitech.eu>
2024-02-06 17:25:54 +0100

    Fix tests

--- GaelO-Flow/src/options/options.module.ts
+++ GaelO-Flow/src/options/options.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OptionsService } from './options.service';
 import { OptionsController } from './options.controller';
 import { Option } from './option.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Option])],
-  providers: [OptionsService],
+  providers: [OptionsService, ConfigService],
   controllers: [OptionsController],
 })
 export class OptionsModule {}
