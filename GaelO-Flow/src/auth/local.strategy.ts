2c11838e2242c5fc1e6fe27c2f043a98859120a9
Antonin <antonin.laudon@epitech.eu>
2024-03-14 11:27:14 +0100

    Fix: login

--- GaelO-Flow/src/auth/local.strategy.ts
+++ GaelO-Flow/src/auth/local.strategy.ts
@@ -6,7 +6,10 @@ import { AuthService } from './auth.service';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-    super();
+    super({
+      usernameField: 'Username',
+      passwordField: 'Password',
+    });
   }
 
   async validate(username: string, password: string): Promise<any> {
