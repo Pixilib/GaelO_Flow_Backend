2cdedebaca5c1f377f45efcb7c765c8273dc94fa
Antonin <antonin.laudon@epitech.eu>
2024-03-14 13:45:46 +0100

    Add: tmtv worker

--- GaelO-Flow/src/processing/processingWorker.ts
+++ GaelO-Flow/src/processing/processingWorker.ts
@@ -1,32 +1,66 @@
 import { Worker, Job } from 'bullmq';
 import OrthancClient from '../orthanc/OrthancClient';
+import { TmtvService } from './tmtv.service';
 import { ConfigService } from '@nestjs/config';
 
 import Redis from 'ioredis';
+import ProcessingClient from './ProcessingClient';
+import { ProcessingMaskEnum } from './processingMask.enum';
 
-async function TmtvInference(
+async function TmtvWorker(
   orthancClient: OrthancClient,
   configService: ConfigService,
+  processingClient: ProcessingClient,
 ) {
   const redisHost = configService.get('REDIS_ADDRESS', 'localhost');
   const redisPort = configService.get('REDIS_PORT', 6379);
   const connectionString = 'redis://' + redisHost + ':' + redisPort;
   const redis = new Redis(connectionString, { maxRetriesPerRequest: null });
-  const tmtvInferenceWorker = new Worker(
+  const TmtvWorker = new Worker(
     'processing',
     async (job: Job) => {
+      const tmtvService = new TmtvService(orthancClient, processingClient);
+      const ctOrthancSeriesId: string = job.data.ctOrthancSeriesId;
+      const ptOrthancSeriesId: string = job.data.ptOrthancSeriesId;
+      const sendMaskToOrthancAs: ProcessingMaskEnum =
+        job.data.sendMaskToOrthancAs;
+      const withFragmentedMask: boolean =
+        job.data.withFragmentedMask == undefined
+          ? false
+          : job.data.withFragmentedMask;
+
+      tmtvService.setCtOrthancSeriesId(ctOrthancSeriesId);
+      tmtvService.setPtOrthancSeriesId(ptOrthancSeriesId);
+
       job.updateProgress(0);
 
-      // TODO: Implement TMTV Inference
+      await tmtvService.sendDicomToProcessing();
+      job.updateProgress(20);
 
+      await tmtvService.createSeries();
+      job.updateProgress(40);
+
+      await tmtvService.runInference(withFragmentedMask);
+      job.updateProgress(60);
+
+      if (sendMaskToOrthancAs == ProcessingMaskEnum.SEG) {
+        await tmtvService.sendMaskAsSegToOrthanc();
+      } else if (sendMaskToOrthancAs == ProcessingMaskEnum.RTSS) {
+        await tmtvService.sendMaskAsRtssToOrthanc();
+      } else {
+        throw new Error('Invalid mask type');
+      }
+      job.updateProgress(80);
+
+      await tmtvService.deleteAllRessources();
       job.updateProgress(100);
     },
     { connection: redis },
   );
 
-  tmtvInferenceWorker.on('failed', (job, err) => {
+  TmtvWorker.on('failed', (job, err) => {
     console.error(`Job ${job.id} failed with error ${err.message}`);
   });
 }
 
-export default TmtvInference;
+export default TmtvWorker;
