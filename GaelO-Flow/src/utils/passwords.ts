52d0f56883d1f8366796b42fb39a470eb358eb16
salim kanoun <salim.kanoun@gmail.Com>
2024-04-05 17:19:49 +0200

    add hash utils

---
+++ GaelO-Flow/src/utils/passwords.ts
@@ -0,0 +1,14 @@
+import * as bcryptjs from 'bcryptjs';
+import * as crypto from 'crypto';
+
+export const hashPassword = async (password: string) => {
+  const saltTest = await bcryptjs.genSalt();
+  const hash = await bcryptjs.hash(password, saltTest);
+  return hash;
+};
+
+export const generateToken = async () => {
+  const token = await crypto.randomBytes(32).toString('hex');
+  const hash = await hashPassword(token);
+  return { token, hash };
+};
