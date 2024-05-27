9e93cfcf44409bce3b04a7e5dc227538fc871b15
Antonin <antonin.laudon@epitech.eu>
2024-05-27 16:07:14 +0200

    Add: new route to get roles from labels

--- GaelO-Flow/src/labels/label.entity.ts
+++ GaelO-Flow/src/labels/label.entity.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Entity, PrimaryColumn } from 'typeorm';
+import { Role } from '../roles/role.entity';
+import { Entity, JoinTable, ManyToMany, PrimaryColumn } from 'typeorm';
 
 @Entity()
 export class Label {
   @ApiProperty({ example: 'name' })
   @PrimaryColumn({ name: 'name' })
   Name: string;
+
+  @ApiProperty({ example: [], required: false, default: [] })
+  @ManyToMany(() => Role, (role) => role.Labels)
+  Roles?: Role[];
 }
